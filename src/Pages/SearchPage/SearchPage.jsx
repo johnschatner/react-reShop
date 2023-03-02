8205e78c022e76f97537f01f11645450265cca24
johnschatner <leontin2001@gmail.com>
2023-03-02 10:29:08 +0100

    Enhance page transitions

--- src/Pages/SearchPage/SearchPage.jsx
+++ src/Pages/SearchPage/SearchPage.jsx
@@ -9,9 +9,9 @@ function SearchPage() {
   return (
     <motion.div
       // Prop that will animate when component is removed from DOM
-      exit={{ opacity: 0 }}
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
+      exit={{ opacity: 0, scale: 0.9 }}
+      initial={{ opacity: 0, scale: 0.9 }}
+      animate={{ opacity: 1, scale: 1 }}
     >
       <div>
         <br />
