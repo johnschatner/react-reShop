beb537d9f5c9e25fa1503d0859c9f3bcddf3dfb2
johnschatner <leontin2001@gmail.com>
2023-03-14 15:43:46 +0100

    Fix search item navigation

--- src/Components/Header/Search/SearchItem.jsx
+++ src/Components/Header/Search/SearchItem.jsx
@@ -1,22 +1,34 @@
 import "./SearchItem.css";
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
 
 // Context
-import { useContext } from "react";
 import { ShopContext } from "../../../context/ReShopContext";
+import { HeaderContext } from "../../../context/ReHeaderContext";
 
 function SearchItem(props) {
   // Context
   const { addToCart } = useContext(ShopContext);
+  const { setHeaderOpen } = useContext(HeaderContext);
   // Product destructuring
   const { id, name, price, thumbnail } = props.product;
 
+  // React router to navigate programmatically
+  const navigate = useNavigate();
+  const productNameUrl = name
+    .toLowerCase()
+    .replaceAll(/\s|[()/]/g, (match) => (match === " " ? "-" : ""))
+    .replace(/-+/g, "-");
+  const navigateToProductPage = () => {
+    navigate(`/product/${productNameUrl}&id=${id}`); // Navigate using useNavigate hook
+    setHeaderOpen(() => false);
+  };
+
   return (
     <div className="search-item">
-      <div className="search-item__left">
+      <div onClick={navigateToProductPage} className="search-item__left">
         <div className="search-item__thumb">
-          <a>
-            <img src={thumbnail} alt={name} />
-          </a>
+          <img src={thumbnail} alt={name} />
         </div>
         <div className="search-item__name">{name}</div>
       </div>
